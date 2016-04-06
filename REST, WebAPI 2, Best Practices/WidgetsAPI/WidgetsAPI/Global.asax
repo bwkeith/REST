bd38442afd26186dd162f5bf9bc9a4b8008a3a3b
Keith Telle <keith@bit-wizards.com>
2016-04-06 09:22:55 -0500

    added existing presentation files

---
+++ REST, WebAPI 2, Best Practices/WidgetsAPI/WidgetsAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WidgetsAPI.WebApiApplication" Language="C#" %>
---
+++ REST, WebAPI 2, Best Practices/WidgetsAPI/WidgetsAPI/Models/Widget.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace WidgetsAPI.Models
+{
+    public class Widget
+    {
+        public string Description { get; set; }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [JsonIgnore]
+        public string Metadata { get; set; }
+    }
+}
